a0df824c21e4e361e827dbf3e89a05e697216f65
porkkamali <kamaliamulu20@gmail.com>
2018-09-04 19:16:40 +0530

    added discovery service

--- DiscoveryService/src/main/java/com/stackroute/tldm/DiscoveryService/DiscoveryServiceApplication.java
+++ DiscoveryService/src/main/java/com/stackroute/tldm/DiscoveryService/DiscoveryServiceApplication.java
@@ -2,8 +2,12 @@ package com.stackroute.tldm.DiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
