a9bafbbef8a2d8bc3c73536034e167c447cefa0a
soudas69 <soudas69@in.ibm.com>
2018-09-05 11:21:44 +0530

    config service changed

--- ConfigurationService/src/main/java/com/stackroute/tldm/ConfigurationService/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/com/stackroute/tldm/ConfigurationService/ConfigurationServiceApplication.java
@@ -2,10 +2,11 @@ package com.stackroute.tldm.ConfigurationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
 	public static void main(String[] args) {
