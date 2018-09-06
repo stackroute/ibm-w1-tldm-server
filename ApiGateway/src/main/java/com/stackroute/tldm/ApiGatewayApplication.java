92b4231d8ec7d764b9061110217be7fcc4c3e93d
pankum96 <pankum96@in.ibm.com>
2018-09-06 20:05:12 +0530

    Apigateway

---
+++ ApiGateway/src/main/java/com/stackroute/tldm/ApiGatewayApplication.java
@@ -0,0 +1,36 @@
+package com.stackroute.tldm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.stackroute.tldm.filter.ErrorFilter;
+import com.stackroute.tldm.filter.PostFilter;
+import com.stackroute.tldm.filter.PreFilter;
+import com.stackroute.tldm.filter.RouteFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
