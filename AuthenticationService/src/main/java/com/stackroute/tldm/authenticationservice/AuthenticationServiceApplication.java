c1fdf649874916be7f3fc48b5ff726ca043b524b
swedhaselva <swedhaselvaraj1996@gmail.com>
2018-09-06 12:05:46 +0530

    modified user service

--- AuthenticationService/src/main/java/com/stackroute/tldm/authenticationservice/AuthenticationServiceApplication.java
+++ AuthenticationService/src/main/java/com/stackroute/tldm/authenticationservice/AuthenticationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.stackroute.tldm.AuthenticationService;
+package com.stackroute.tldm.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
