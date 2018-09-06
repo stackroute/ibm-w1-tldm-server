2f08c07f53b945e6a8bab47c8ed198c09dc940eb
Rajashree <rajitheprincess@gmail.com>
2018-09-06 20:52:51 +0530

    Gateway-service checked working

--- GatewayService/src/main/java/com/stackroute/tldm/GatewayServiceApplication.java
+++ GatewayService/src/main/java/com/stackroute/tldm/GatewayServiceApplication.java
@@ -3,10 +3,36 @@ package com.stackroute.tldm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GatewayServiceApplication {
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import com.stackroute.tldm.filter.ErrorFilter;
+import com.stackroute.tldm.filter.PostFilter;
+import com.stackroute.tldm.filter.PreFilter;
+import com.stackroute.tldm.filter.RouteFilter;
+
+@SpringBootApplication
+
+@EnableZuulProxy
+public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+@Bean
+   public PreFilter preFilter() {
+       return new PreFilter();
+   }
+   @Bean
+   public PostFilter postFilter() {
+       return new PostFilter();
+   }
+   @Bean
+   public ErrorFilter errorFilter() {
+       return new ErrorFilter();
+   }
+   @Bean
+   public RouteFilter routeFilter() {
+       return new RouteFilter();
+   }
+
 }
