61cb15aba58151f2be016625c598dcd36cc89e05
swedhaselva <swedhaselvaraj1996@gmail.com>
2018-09-11 19:49:28 +0530

    test cases

--- UserService/src/main/java/com/stackroute/tldm/service/UserServiceImpl.java
+++ UserService/src/main/java/com/stackroute/tldm/service/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.stackroute.tldm.service;
 
-
-
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -16,73 +14,81 @@ import com.stackroute.tldm.repository.UserRepository;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private UserRepository userRepo;
+	private UserRepository userRepo;
 
-    @Autowired
-    public UserServiceImpl(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
+	@Autowired
+	public UserServiceImpl(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
 
-    public boolean registerUser(UserModel user) throws UserAlreadyExistsException {
-        boolean flag = false;
-        if (userRepo.getUserByUserName(user.getUserName()) != null) {
-            throw new UserAlreadyExistsException("user exists");
-        } else {
-            userRepo.insert(user);
-            flag = true;
-        }
-        return flag;
+	public boolean registerUser(UserModel user) throws UserAlreadyExistsException {
+		boolean flag = false;
+		if (userRepo.getUserByUserName(user.getUserName()) != null) {
+			throw new UserAlreadyExistsException("user exists");
+		} else {
+			userRepo.insert(user);
+			flag = true;
+		}
+		return flag;
 
-    }
+	}
 
-    public UserModel updateUser(String movieId, UserModel user) throws UserNotFoundException {
-        UserModel fetch = userRepo.findById(user.getUserId()).get();
-        if (fetch != null) {
-            userRepo.save(user);
+	public UserModel updateUser(String movieId, UserModel user) throws UserNotFoundException {
+		UserModel fetch = userRepo.findById(user.getUserId()).get();
+		if (fetch != null) {
+			userRepo.save(user);
 
-        } else {
-            throw new UserNotFoundException("user not found");
-        }
-        return user;
-    }
+		} else {
+			throw new UserNotFoundException("user not found");
+		}
+		return user;
+	}
 
-    public boolean deleteUser(String userId) throws UserNotFoundException {
-        boolean flag = false;
-        if (userRepo.findById(userId) != null) {
-            userRepo.deleteById(userId);
-            flag = true;
-        } else {
-            throw new UserNotFoundException("user not found");
-        }
+	public boolean deleteUser(String userId) throws UserNotFoundException {
+		boolean flag = false;
+		if (userRepo.findById(userId) != null) {
+			userRepo.deleteById(userId);
+			flag = true;
+		} else {
+			throw new UserNotFoundException("user not found");
+		}
 
-        return flag;
-    }
+		return flag;
+	}
 
-    public UserModel getUserById(String userId) throws UserNotFoundException {
-    	UserModel fetchUser;
-    	try {
-        fetchUser = userRepo.findById(userId).get();
-    	}
-    	catch(NoSuchElementException exception){
-    		throw new UserNotFoundException("User not found");
-    	}
-        return fetchUser;
-    }
+	public UserModel getUserById(String userId) throws UserNotFoundException {
+		UserModel fetchUser;
+		try {
+			fetchUser = userRepo.findById(userId).get();
+		} catch (NoSuchElementException exception) {
+			throw new UserNotFoundException("User not found");
+		}
+		return fetchUser;
+	}
 
-    public UserModel getUserByUserName(String userName) throws UserNotFoundException {
-    	UserModel user;
-    	try {
-        user = userRepo.getUserByUserName(userName);
-    	}
-    	catch(NoSuchElementException exception) {
-    		 throw new UserNotFoundException("User not found");
-    	}
-        return user;
-    }
+	public UserModel getUserByUserName(String userName) throws UserNotFoundException {
+		UserModel user;
+		try {
+			user = userRepo.getUserByUserName(userName);
+		} catch (NoSuchElementException exception) {
+			throw new UserNotFoundException("User not found");
+		}
+		return user;
+	}
 
-    
-    public List<UserModel> getAllUsers(){
-        List<UserModel> userList = userRepo.findAll();
-        return userList;
-    }
+	@Override
+	public UserModel getUserByName(String name) throws UserNotFoundException {
+		UserModel user;
+		try {
+			user = userRepo.getUserByName(name);
+		} catch (NoSuchElementException exception) {
+			throw new UserNotFoundException("User not found");
+		}
+		return user;
+	}
+
+	public List<UserModel> getAllUsers() {
+		List<UserModel> userList = userRepo.findAll();
+		return userList;
+	}
 }
