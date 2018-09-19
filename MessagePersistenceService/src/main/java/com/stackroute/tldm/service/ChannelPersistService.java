36a5c6ad82ad72d63c66f0fc0f594634ae82c59e
vetris11 <vetris11@in.ibm.com>
2018-09-19 20:04:00 +0530

    fixed message persistence service

--- MessagePersistenceService/src/main/java/com/stackroute/tldm/service/ChannelPersistService.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.tldm.service;
-
-import com.stackroute.tldm.model.ChannelMessage;
-
-public interface ChannelPersistService {
-
-	void saveMessage(ChannelMessage channelMessage);
-
-}
