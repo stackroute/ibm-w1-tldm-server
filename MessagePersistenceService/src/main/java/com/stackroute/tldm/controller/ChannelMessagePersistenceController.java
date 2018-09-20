67669c358780ad3c97a1d0cfb96e66f2064b1257
porkkamali <kamaliamulu20@gmail.com>
2018-09-21 03:06:46 +0530

    adding rest api's, repositories, service from MessageService to MessagePersistenceService

--- MessagePersistenceService/src/main/java/com/stackroute/tldm/controller/ChannelMessagePersistenceController.java
+++ MessagePersistenceService/src/main/java/com/stackroute/tldm/controller/ChannelMessagePersistenceController.java
@@ -1,7 +1,7 @@
 package com.stackroute.tldm.controller;
+
 import java.util.UUID;
 
-import com.stackroute.tldm.service.ChannelMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,28 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stackroute.tldm.exception.MessageNotFoundException;
-//import com.stackroute.tldm.model.ChannelMessage;
-//import com.stackroute.tldm.service.ChannelPersistService;
-import com.stackroute.tldm.service.MessageService;
+import com.stackroute.tldm.service.ChannelMessageService;
 
 @RestController
-@RequestMapping("/api/v1/message")
+@RequestMapping("api/v1/channel-message")
 @CrossOrigin("*")
-public class MessagePersistenceController {
-	
-	private MessageService messageService;
+public class ChannelMessagePersistenceController {
+
+	private ChannelMessageService channelMessageService;
 
 	@Autowired
-	public MessagePersistenceController(MessageService messageService, ChannelMessageService channelMessageService) {
-		this.messageService = messageService;
+	public ChannelMessagePersistenceController(ChannelMessageService channelMessageService) {
+		this.channelMessageService = channelMessageService;
 	}
 
-	// Delete a particular messageById.
-	@DeleteMapping("/{messageId}")
-	public ResponseEntity<?> deleteMessage(@PathVariable("messageId") UUID messageId) {
+	// Delete a particular Channel Message
+	@DeleteMapping("/{messageId}/{senderId}")
+	public ResponseEntity<?> deleteChannelMessages(@PathVariable("messageId") UUID messageId,
+			@PathVariable("senderId") String senderId) {
 		ResponseEntity<?> responseEntity;
 		try {
-			if (messageService.deleteMessage(messageId)) {
+			if (channelMessageService.deleteChannelMessage(messageId, senderId)) {
 				responseEntity = new ResponseEntity<>(HttpStatus.OK);
 			} else {
 				responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -45,19 +44,17 @@ public class MessagePersistenceController {
 		return responseEntity;
 	}
 
-	// Get a conversation between Sender and the Receiver.
-	@GetMapping("/{senderId}/{receiverId}")
-	public ResponseEntity<?> getMessagesBySenderAndReceiver(@PathVariable("senderId") String senderId,
-			@PathVariable("receiverId") String receiverId) {
+	// Get Channel Messages By ChannelId
+	@GetMapping("/{channelId}")
+	public ResponseEntity<?> getChannelMessagesByChannelId(@PathVariable("channelId") String channelId) {
 		ResponseEntity<?> responseEntity;
 		try {
-			responseEntity = new ResponseEntity<>(messageService.getMessagesBySenderIdAndReceiverId(senderId, receiverId),
+			responseEntity = new ResponseEntity<>(channelMessageService.getChannelMessagesByChannelId(channelId),
 					HttpStatus.OK);
 		} catch (MessageNotFoundException e) {
-			responseEntity = new ResponseEntity<>("Message Not Found!", HttpStatus.NOT_FOUND);
+			responseEntity = new ResponseEntity<>("message Not Found!", HttpStatus.NOT_FOUND);
 		}
-
 		return responseEntity;
-	}	
+	}
 
 }
