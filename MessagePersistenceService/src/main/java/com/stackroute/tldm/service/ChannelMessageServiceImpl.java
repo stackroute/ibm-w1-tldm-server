4818f84506b2f5be84c23ea9e903d9a6b2fe4ac0
soudas69 <soudas69@in.ibm.com>
2018-09-24 12:53:27 +0530

    fixed errors

--- MessagePersistenceService/src/main/java/com/stackroute/tldm/service/ChannelMessageServiceImpl.java
+++ MessagePersistenceService/src/main/java/com/stackroute/tldm/service/ChannelMessageServiceImpl.java
@@ -15,68 +15,59 @@ import java.util.UUID;
 @Service
 public class ChannelMessageServiceImpl implements ChannelMessageService {
 
-	private ChannelChatRepository channelChatRepository;
+    private ChannelChatRepository channelChatRepository;
 
-	@Autowired
-	public ChannelMessageServiceImpl(ChannelChatRepository channelChatRepository) {
-		this.channelChatRepository = channelChatRepository;
-	}
-<<<<<<< HEAD
-	
-	
+    @Autowired
+    public ChannelMessageServiceImpl(ChannelChatRepository channelChatRepository) {
+        this.channelChatRepository = channelChatRepository;
+    }
+
     // this method is used to save a message in a channel
-=======
-    
-	// Saving a Channel Message
->>>>>>> 1ae668e48fb4fcf3b649eb1207590a34199da370
-	@Override
-	public ChannelMessage saveMessage(ChannelMessage channelMessage) {
-		return channelChatRepository.insert(channelMessage);
-	}
-<<<<<<< HEAD
 
-	// this method is used to delete the channel messages
-=======
-    
-	// Deleting a particular Channel Message By Sender
->>>>>>> 1ae668e48fb4fcf3b649eb1207590a34199da370
-	@Override
-	public boolean deleteChannelMessage(UUID messageId, String senderId) throws MessageNotFoundException {
-		if (channelChatRepository.existsById(messageId)) {
-			ChannelMessage message = channelChatRepository.findById(messageId).get();
-			String sender = message.getSender().getUserId();
-			if (sender.equals(senderId) && message != null) {
-				channelChatRepository.deleteById(messageId);
-				return true;
-			} else {
-				throw new MessageNotFoundException("Message Not found!!");
-			}
-		} else {
-			throw new MessageNotFoundException("Message Not found!!");
-		}
-	}
+    // Saving a Channel Message
+    @Override
+    public ChannelMessage saveMessage(ChannelMessage channelMessage) {
+        return channelChatRepository.insert(channelMessage);
+    }
 
-<<<<<<< HEAD
-	// this method is used to get all the channel messages using specific channel ID
-=======
-	// Get All Channel Messages By channelId
->>>>>>> 1ae668e48fb4fcf3b649eb1207590a34199da370
-	@Override
-	public List<ChannelMessage> getChannelMessagesByChannelId(String channelId) throws MessageNotFoundException {
-		List<ChannelMessage> channelMessages = channelChatRepository.findAll();
-		List<ChannelMessage> messageList = new ArrayList<>();
-		if (channelMessages != null) {
-			Iterator<ChannelMessage> messageIterator = channelMessages.iterator();
-			while (messageIterator.hasNext()) {
-				ChannelMessage groupMessages = messageIterator.next();
-				String channel = groupMessages.getChannel().getChannelId();
-				if ((channelId.equals(channel))) {
-					messageList.add(groupMessages);
-				}
-			}
-			return messageList;
-		} else {
-			throw new MessageNotFoundException("Message Not Found!!");
-		}
-	}
+    // this method is used to delete the channel messages
+
+    // Deleting a particular Channel Message By Sender
+    @Override
+
+    public boolean deleteChannelMessage(UUID messageId, String senderId) throws MessageNotFoundException {
+        if (channelChatRepository.existsById(messageId)) {
+            ChannelMessage message = channelChatRepository.findById(messageId).get();
+            String sender = message.getSender().getUserId();
+            if (sender.equals(senderId) && message != null) {
+                channelChatRepository.deleteById(messageId);
+                return true;
+            } else {
+                throw new MessageNotFoundException("Message Not found!!");
+            }
+        } else {
+            throw new MessageNotFoundException("Message Not found!!");
+        }
+    }
+
+    // this method is used to get all the channel messages using specific channel ID
+    // Get All Channel Messages By channelId
+    @Override
+    public List<ChannelMessage> getChannelMessagesByChannelId(String channelId) throws MessageNotFoundException {
+        List<ChannelMessage> channelMessages = channelChatRepository.findAll();
+        List<ChannelMessage> messageList = new ArrayList<>();
+        if (channelMessages != null) {
+            Iterator<ChannelMessage> messageIterator = channelMessages.iterator();
+            while (messageIterator.hasNext()) {
+                ChannelMessage groupMessages = messageIterator.next();
+                String channel = groupMessages.getChannel().getChannelId();
+                if ((channelId.equals(channel))) {
+                    messageList.add(groupMessages);
+                }
+            }
+            return messageList;
+        } else {
+            throw new MessageNotFoundException("Message Not Found!!");
+        }
+    }
 }
