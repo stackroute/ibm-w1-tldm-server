7e1e6b07a9d0ba637e9a3f54232f079f54259ea1
vetris11 <vetris11@in.ibm.com>
2018-09-24 12:33:33 +0530

    comments added in model of MessageService

--- MessageService/src/main/java/com/stackroute/tldm/model/User.java
+++ MessageService/src/main/java/com/stackroute/tldm/model/User.java
@@ -4,12 +4,25 @@ import java.util.Date;
 
 // Model for User.
 
+/*
+ * The class "User" will be acting as the data model for the User Table in the database. 
+ * Java object to recreate it as a table in your database.
+ */
+
 public class User {
 	private String userId;
 	private String userName;
 	private String userMail;
 	private String phoneNum;
 	private Date createdAt;
+	
+	 /*
+     * This class should have five fields
+     * (userId, userName, userMail, phoneNum, createdAt)
+     * This class should
+	 * also contain the getters and setters for the fields,
+	 * parameterized constructor and toString method.
+     */
 
 	public User() {
 	}
