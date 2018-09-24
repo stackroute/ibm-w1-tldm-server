03061ba3af427fd6d32d43e984de3691d706e13a
swedhaselva <swedhaselvaraj1996@gmail.com>
2018-09-24 12:33:37 +0530

    dead codes removed

--- UserService/src/main/java/com/stackroute/tldm/service/UserServiceImpl.java
+++ UserService/src/main/java/com/stackroute/tldm/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import com.stackroute.tldm.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
