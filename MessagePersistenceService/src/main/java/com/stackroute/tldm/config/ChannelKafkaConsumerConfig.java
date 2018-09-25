5134ed65b6ecd7b2f7e53b341b12639b36cc7025
manikg11 <manikg11@in.ibm.com>
2018-09-25 11:23:10 +0530

    modified consumer configurations in message-persistence-service and added search-service structure

--- MessagePersistenceService/src/main/java/com/stackroute/tldm/config/ChannelKafkaConsumerConfig.java
+++ MessagePersistenceService/src/main/java/com/stackroute/tldm/config/ChannelKafkaConsumerConfig.java
@@ -1,8 +1,5 @@
 package com.stackroute.tldm.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.stackroute.tldm.model.ChannelMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -15,35 +12,31 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import com.stackroute.tldm.model.Message;
-
-// Consumer configuration class for User-Messages and Channel-Messages
-// Fetching group-id from cloud configurations.
-// For JSON object to be sent through kafka we need this configuration.
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableKafka
-public class KafkaConsumerConfig {
+public class ChannelKafkaConsumerConfig {
 
     @Value("${server.boot}")
     private String ipConfig;
-	
-	@Bean
-    public ConsumerFactory<String, Message> consumerFactory() {
-        Map<String, Object> config = new HashMap<>();
 
+    @Bean
+    public ConsumerFactory<String, ChannelMessage> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, ipConfig);
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "${groupId1.boot}");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "${groupId2.boot}");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(Message.class));
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new JsonDeserializer<>(ChannelMessage.class));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Message> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Message> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, ChannelMessage> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ChannelMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
-
 }
