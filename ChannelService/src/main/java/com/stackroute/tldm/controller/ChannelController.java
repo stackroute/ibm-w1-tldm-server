7927f4b9c2fd4934f12d50362ed088f27d123441
pankum96 <pankum96@in.ibm.com>
2018-09-25 12:58:45 +0530

    added Swagger Documentation In Channel Service

--- ChannelService/src/main/java/com/stackroute/tldm/controller/ChannelController.java
+++ ChannelService/src/main/java/com/stackroute/tldm/controller/ChannelController.java
@@ -2,6 +2,8 @@ package com.stackroute.tldm.controller;
 
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,7 @@ import com.stackroute.tldm.service.ChannelService;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/v1/channel")
+@Api(value="Channel Service Resource")
 public class ChannelController {
 
 	private ChannelService channelService;
@@ -35,7 +38,10 @@ public class ChannelController {
 	}
 
 	// this handler method is mapped to the URL using HTTP POST method //
+
+
 	@PostMapping
+	@ApiOperation("Create new channels")
 	public ResponseEntity<?> createChannel(@RequestBody Channel channel) {
 		ResponseEntity<?> responseEntity = null;
 		try {
@@ -54,6 +60,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/{channelId}" using HTTP DELETE
 	// method //
 	@DeleteMapping("/{channelId}")
+	@ApiOperation("Delete  existing channel")
 	public ResponseEntity<?> deleteChannel(@PathVariable String channelId) {
 		ResponseEntity<?> responseEntity = null;
 		try {
@@ -68,6 +75,7 @@ public class ChannelController {
 
 	// this handler method is mapped to the URL using HTTP GET method //
 	@GetMapping
+	@ApiOperation("Get all channel list")
 	public ResponseEntity<?> getAllchannels() {
 		ResponseEntity<?> responseEntity;
 		List<Channel> channelList = channelService.getAllChannels();
@@ -83,6 +91,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/update/{channelId}" using HTTP PUT
 	// method //
 	@PutMapping("/update/{channelId}")
+	@ApiOperation("Update channels details by channel Id")
 	public ResponseEntity<?> updateChannel(@PathVariable String channelId, @RequestBody Channel channel) {
 		ResponseEntity<?> responseEntity = null;
 		try {
@@ -99,6 +108,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/{channelId}" using HTTP PUT method
 	// //
 	@PutMapping("/{channelId}")
+	@ApiOperation("Update users in channel")
 	public ResponseEntity<?> updateChannelUser(@PathVariable String channelId, @RequestBody User user) {
 		ResponseEntity<?> responseEntity = null;
 		try {
@@ -115,6 +125,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/{channelName}" using HTTP GET
 	// method //
 	@GetMapping("/{channelName}")
+	@ApiOperation("Get Detail of Channel by Channel Name")
 	public ResponseEntity<?> getChannelByChannelName(@PathVariable String channelName) {
 		ResponseEntity<?> responseEntity;
 		Channel fetch;
@@ -152,6 +163,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/getuser/{channelName}" using HTTP
 	// GET method"
 	@GetMapping("/getuser/{channelName}")
+	@ApiOperation("get the user of the particular channel by providing channel name")
 	public ResponseEntity<?> getUsersByChannelName(@PathVariable String channelName) {
 		ResponseEntity<?> responseEntity;
 		List<User> allUsers;
@@ -168,6 +180,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/user/{userName}" using HTTP GET
 	// method //
 	@GetMapping("/user/{userName}")
+	@ApiOperation("Get the Channel by providing name of user")
 	public ResponseEntity<?> getchannelByUserName(@PathVariable String userName) {
 		ResponseEntity<?> responseEntity;
 		List<Channel> fetchChannel = channelService.getListOfChannelsByUsers(userName);
@@ -182,6 +195,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/{channelId}/{userId}" using HTTP
 	// DELETE method //
 	@DeleteMapping("/{channelId}/{userId}")
+	@ApiOperation("Remove a user from particular Channel by providing userId and channel Id")
 	public ResponseEntity<?> removeChannelUser(@PathVariable String channelId, @PathVariable String userId) {
 		ResponseEntity<?> responseEntity = null;
 		
@@ -196,6 +210,7 @@ public class ChannelController {
 	// this handler method is mapped to the URL "/users/{userId}" using HTTP GET
 	// method //
 	@GetMapping("/users/{userId}")
+	@ApiOperation("Get list of channel in which the user is present")
 	public ResponseEntity<?> getchannelByUserId(@PathVariable String userId) {
 		ResponseEntity<?> responseEntity;
 		List<Channel> fetchChannel = channelService.getListOfChannelsByUser(userId);
