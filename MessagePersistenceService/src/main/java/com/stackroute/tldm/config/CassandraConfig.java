42f9f284f1a1ebc3a751d6d4dfb2b33e9ac14856
soudas69 <soudas69@in.ibm.com>
2018-09-26 16:51:08 +0530

    Cassandra config changed

--- MessagePersistenceService/src/main/java/com/stackroute/tldm/config/CassandraConfig.java
+++ MessagePersistenceService/src/main/java/com/stackroute/tldm/config/CassandraConfig.java
@@ -4,35 +4,42 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-public class CassandraConfig {
-	
-	// message_key_space is a namespace used in replication on nodes
-	public static final String KEYSPACE = "message_key_space";
+@EnableCassandraRepositories
+public class CassandraConfig extends AbstractCassandraConfiguration {
 
-	// Enum identifying any schema actions to take at startup.
-	public SchemaAction getSchemaAction() {
-		return SchemaAction.CREATE_IF_NOT_EXISTS;
-	}
+    // message_key_space is a namespace used in replication on nodes
+    public static final String KEYSPACE = "message_key_space";
 
-	// Object to configure a CREATE KEYSPACE specification
-	protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-		CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+    // Enum identifying any schema actions to take at startup.
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
 
-		return Arrays.asList(specification);
-	}
+    // Object to configure a CREATE KEYSPACE specification
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE)
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication();
+        return Arrays.asList(specification);
+    }
 
-	// returns the keyspace used
-	protected String getKeyspaceName() {
-		return KEYSPACE;
-	}
+    // returns the keyspace used
+    protected String getKeyspaceName() {
+        return KEYSPACE;
+    }
 
-	// returns the specific package used
-	public String[] getEntityBasePackages() {
-		return new String[] { "com.stackroute.tldm" };
-	}
+    // returns the specific package used
+    public String[] getEntityBasePackages() {
+        return new String[]{"com.stackroute.tldm"};
+    }
 
 }
